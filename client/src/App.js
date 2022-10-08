4106a42241d6055f7fde6f27cdcd26519136264b
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2022-10-08 16:43:54 +0200

    get all landings in map working

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,46 @@
 import React from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
+import { landingsContext } from './context/landingsContext';
+import axios from 'axios';
 
 
 function App() {
+  const [landings, setLandings] = useState([]);//Almacenar fetch de all landings
+
+  useEffect(() => {
+    getLandings()
+  }, [])
+
+  const getLandings = async () => {
+    try {
+      //Hacer fetch al back
+      const { data } = await axios.get(`/api/astronomy/landings/`);
+      setLandings(data);
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const data = {
+    landings,
+    setLandings
+  };
+
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Main />
-        <Footer />
-      </BrowserRouter>
+      <landingsContext.Provider value={data}>
+        <BrowserRouter>
+          <Header />
+          <Main />
+          <Footer />
+        </BrowserRouter>
+      </landingsContext.Provider>
     </div>
   );
 }
--- client/src/components/Main/Landing/Landing.jsx
+++ client/src/components/Main/Landing/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useContext } from 'react';
+import { landingsContext } from '../../../context/landingsContext';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import axios from "axios";
 import L from 'leaflet';
@@ -9,8 +10,9 @@ import { Link } from 'react-router-dom';
 
 
 
-function Landing() {
 
+function Landing() {
+  const { landings, setLandings } = useContext(landingsContext);//Almacenar fetch de all landings
 
 
   var icon = new L.Icon({
@@ -20,32 +22,9 @@ function Landing() {
   });
   const map = { "width": "100%", "height": "50vh" };
 
-  useEffect(() => {
-    getLandings()
-  }, [])
-
-  const getLandings = async () => {
-    try {
-      //Hacer fetch al back
-      const { data } = await axios.get(`/api/astronomy/landings/`);
-      const newLanding = {
-        name: data.name,
-        id: data.id,
-        nametype: data.nametype,
-        recclass: data.recclass,
-        mass: data.mass,
-        fall: data.fall,
-        year: data.year,
-        reclat: data.reclat,
-        relong: data.relong,
-        geolocation: data.geolocation,
-      }
-
-      console.log(data);
-    } catch (error) {
-      console.log(error)
-    }
-
+  const log = landings[0].geolocation.latitude;
+  if (log) {
+    console.log(log);
   }
 
   return (<>
@@ -81,30 +60,30 @@ function Landing() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.775, 6.08333]} icon={icon} >
-          <Popup>
-            "name": "Aachen",
-            "id": "1",
-            "nametype": "Valid",
-            "recclass": "L5",
-            "mass": "21",
-            "fall": "Fell",          </Popup>
-        </Marker>
+        {landings.map((landing, i) => landing.geolocation && landing.reclat && landing.reclong ? (
+
+          <Marker
+            key={i}
+            position={[landing.geolocation.latitude, landing.geolocation.longitude]}
+            icon={icon}>
+            <Popup>Detalles:
+              <ul>
+                <li>Nombre: {landing.name}</li>
+                <li>ID: {landing.id}</li>
+                <li>Clase: {landing.recclass}</li>
+                <li>Masa: {landing.mass} kg</li>
+                <li>Fecha: {landing.year}</li>
+                <li>Latitud: {landing.reclat}</li>
+                <li>Longitud: {landing.reclong}</li>
+              </ul>
+            </Popup>
+          </Marker>
+        ) : null)}
 
 
-        {/* Borrar y meter map del fetch del back */}
 
-        <Marker position={[56.18333, 10.23333]} icon={icon} >
-          <Popup>
-            "name": "Aachen",
-            "id": "1",
-            "nametype": "Valid",
-            "recclass": "L5",
-            "mass": "21",
-            "fall": "Fell",          </Popup>
-        </Marker>
+
       </MapContainer>
-      {/* Borrar y meter map del fetch del back */}
 
     </div>
     <div>
