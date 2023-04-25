20344b63b189a45cabdc886cbc84e2ba3254a95b
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2023-04-25 17:33:36 +0200

    port

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 //Configurations
-app.set("port", process.env.PORT || 80);
+app.set("port", process.env.PORT || 5000);
 // const port = process.env.PORT || 5000;
 
 //Middlewares
