bf1e68cfdb708e7d877e8645ad12af90de59178b
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2023-04-25 14:43:31 +0200

    add / route

--- server/app.js
+++ server/app.js
@@ -44,6 +44,11 @@ const loggerFormat =
 //   })
 // );
 
+//Ruta "/"
+app.get("/", (req, res) => {
+  res.send("Hola, mundo!");
+});
+
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
